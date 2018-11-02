7a0f00e7b7b01cfff048e184d4511b774d51001f
Christian <christian.auderset@alphasquare.ch>
2018-11-02 16:23:59 +0100

    Wording of get_filter Function updated

--- bikeshare.py
+++ bikeshare.py
@@ -7,7 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 def get_filters():
-    """Asks user to specify a city, month, and day to analyze.
+    """Asks user to specify a city, a month and a day to analyze.
     Returns:
         city - name of the city to analyze
         month - index of month or all
